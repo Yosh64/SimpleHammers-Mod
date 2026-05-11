c97289f5fa83dc10a9b79ef0c4b429164d93f319
Yosh64 <65314603+Yosh64@users.noreply.github.com>
2026-05-11 22:42:41 +0200

    Removed duplicate ToolMaterials, Removed Emerald Tier, Removed Hammer Hand and Handles as not needed, Changed recipes/advancements in consequences

--- src/main/java/net/withrage/simplehammers/items/custom/ModToolMaterial.java
+++ src/main/java/net/withrage/simplehammers/items/custom/ModToolMaterial.java
@@ -3,84 +3,18 @@ package net.withrage.simplehammers.items.custom;
 import net.minecraft.block.Block;
 import net.minecraft.item.Items;
 import net.minecraft.item.ToolMaterial;
-import net.minecraft.item.ToolMaterials;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.registry.tag.BlockTags;
-import net.minecraft.registry.tag.ItemTags;
 import net.minecraft.registry.tag.TagKey;
 import net.withrage.simplehammers.config.SimpleHammersConfig;
 
 public class ModToolMaterial {
-
-    public static final ToolMaterial WOOD = new ToolMaterial() {
-        @Override public int getDurability() { return SimpleHammersConfig.woodenDurability; }
-        @Override public float getMiningSpeedMultiplier() { return 6.0F; }
-        @Override public float getAttackDamage() { return 1.0F; }
-        @Override public int getEnchantability() { return 8; }
-        @Override public Ingredient getRepairIngredient() { return Ingredient.fromTag(ItemTags.PLANKS); }
-        @Override public TagKey<Block> getInverseTag() { return BlockTags.INCORRECT_FOR_WOODEN_TOOL; }
-    };
-
-    public static final ToolMaterial STONE = new ToolMaterial() {
-        @Override public int getDurability() { return SimpleHammersConfig.stoneDurability; }
-        @Override public float getMiningSpeedMultiplier() { return 6.0F; }
-        @Override public float getAttackDamage() { return 1.0F; }
-        @Override public int getEnchantability() { return 9; }
-        @Override public Ingredient getRepairIngredient() { return Ingredient.fromTag(ItemTags.STONE_TOOL_MATERIALS); }
-        @Override public TagKey<Block> getInverseTag() { return BlockTags.INCORRECT_FOR_STONE_TOOL; }
-    };
-
     public static final ToolMaterial COPPER = new ToolMaterial() {
-        @Override public int getDurability() { return SimpleHammersConfig.copperDurability; }
-        @Override public float getMiningSpeedMultiplier() { return 6.0F; }
-        @Override public float getAttackDamage() { return 1.0F; }
-        @Override public int getEnchantability() { return 10; }
-        @Override public Ingredient getRepairIngredient() { return Ingredient.ofItems(Items.COPPER_INGOT); }
-        @Override public TagKey<Block> getInverseTag() { return BlockTags.INCORRECT_FOR_IRON_TOOL; }
-    };
-
-    public static final ToolMaterial GOLD = new ToolMaterial() {
-        @Override public int getDurability() { return SimpleHammersConfig.goldenDurability; }
-        @Override public float getMiningSpeedMultiplier() { return 6.0F; }
-        @Override public float getAttackDamage() { return 1.0F; }
-        @Override public int getEnchantability() { return 11; }
-        @Override public Ingredient getRepairIngredient() { return Ingredient.ofItems(Items.GOLD_INGOT); }
-        @Override public TagKey<Block> getInverseTag() { return BlockTags.INCORRECT_FOR_GOLD_TOOL; }
-    };
-
-    public static final ToolMaterial IRON = new ToolMaterial() {
-        @Override public int getDurability() { return SimpleHammersConfig.ironDurability; }
-        @Override public float getMiningSpeedMultiplier() { return 6.0F; }
-        @Override public float getAttackDamage() { return 2.0F; }
-        @Override public int getEnchantability() { return 12; }
-        @Override public Ingredient getRepairIngredient() { return Ingredient.ofItems(Items.IRON_INGOT); }
-        @Override public TagKey<Block> getInverseTag() { return BlockTags.INCORRECT_FOR_IRON_TOOL; }
-    };
-
-    public static final ToolMaterial EMERALD = new ToolMaterial() {
-        @Override public int getDurability() { return SimpleHammersConfig.emeraldDurability; }
-        @Override public float getMiningSpeedMultiplier() { return 9.0F; }
-        @Override public float getAttackDamage() { return 3.0F; }
-        @Override public int getEnchantability() { return 18; }
-        @Override public Ingredient getRepairIngredient() { return Ingredient.ofItems(Items.EMERALD); }
-        @Override public TagKey<Block> getInverseTag() { return BlockTags.INCORRECT_FOR_DIAMOND_TOOL; }
-    };
-
-    public static final ToolMaterial DIAMOND = new ToolMaterial() {
-        @Override public int getDurability() { return SimpleHammersConfig.diamondDurability; }
-        @Override public float getMiningSpeedMultiplier() { return 7.0F; }
-        @Override public float getAttackDamage() { return 3.0F; }
-        @Override public int getEnchantability() { return 16; }
-        @Override public Ingredient getRepairIngredient() { return Ingredient.ofItems(Items.DIAMOND); }
-        @Override public TagKey<Block> getInverseTag() { return BlockTags.INCORRECT_FOR_DIAMOND_TOOL; }
-    };
-
-    public static final ToolMaterial NETHERITE = new ToolMaterial() {
-        @Override public int getDurability() { return SimpleHammersConfig.netheriteDurability; }
-        @Override public float getMiningSpeedMultiplier() { return 8.0F; }
-        @Override public float getAttackDamage() { return 4.0F; }
-        @Override public int getEnchantability() { return 16; }
-        @Override public Ingredient getRepairIngredient() { return Ingredient.ofItems(Items.NETHERITE_INGOT); }
-        @Override public TagKey<Block> getInverseTag() { return BlockTags.INCORRECT_FOR_NETHERITE_TOOL; }
+        public int getDurability() { return SimpleHammersConfig.copperDurability; }
+        public float getMiningSpeedMultiplier() { return 6.0F; }
+        public float getAttackDamage() { return 1.0F; }
+        public int getEnchantability() { return 10; }
+        public Ingredient getRepairIngredient() { return Ingredient.ofItems(Items.COPPER_INGOT); }
+        public TagKey<Block> getInverseTag() { return BlockTags.INCORRECT_FOR_IRON_TOOL; }
     };
 }
